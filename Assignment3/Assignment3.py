72eaaf95d05c2c735dd2f49c137c1627982464d9
Mark Ariniello <mark.ariniello@gmail.com>
2015-09-18 15:51:16 -0600

    finalish commit

--- Assignment3/Assignment3.py
+++ Assignment3/Assignment3.py
@@ -5,7 +5,7 @@ with open(sys.argv[1], "r") as world:
     for line in world.readlines(): 
         maze.append(line.rstrip('\n').split(' ')) 
          
-print maze 
+#print maze 
 
 class Node: 
 
@@ -24,19 +24,36 @@ class Graph:
         self.start = Node(7,0)
         self.end = Node(0, 9)
 
-    def astar(self, graph, start, end): 
+    def astar(self): 
         curropen.append(start)
         closed = []
         currnode = start
+        lowestnode = start
+        x = 20
         while len(curropen) != 0:
             for node in curropen: 
                 if node.toHere < x:
                     x = node.toHere + node.toGo
+                open.remove
             curropen.remove(currnode)
             if currnode != end: 
                 closed.append(currnode)
-                for i = -1 to i = 1: 
-                    for j = -1 to j = 1:
+                for i in range (-1, 1): 
+                    for j in range (-1, 1):
                         if (currnode.x + i) >= 0 and (currnode.x + i) <= 9 and (currnode.y + j) >= 0 and (currnode.y + j) <= 7 and (i !=0 and j !=0) and maze[currnode.x + i][currnode.y + j] != 2: 
                             node = Node(currnode.x + i, currnode.y + j, currnode, currnode.toHere + 0) 
-                            if i == 0 or j == 0: 
+                            if i == 0 or j == 0:
+                                node = Node(currnode.x + i, currnode.y + j, currnode, currnode.toHere + 10)
+                            if i != 0 and j != 0: 
+                                node = Node(currnode.x + i, currnode.y + j, currnode, currnode.toHere + 14)
+                            if maze[currnode.x + i][currnode.y + j] == 1: 
+                                node.toHere = node.toHere + 10
+                            if node.cost < lowestnode.cost and (node.x != 0 and node.y != 0): 
+                                lowestnode = node
+                open.append(node)
+            else: 
+                return closed        
+        print closed 
+
+x = Graph(maze)
+x.astar()  
