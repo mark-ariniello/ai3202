0b70c4089a865f54843c5ab809a02f28e8b21b2c
Mark Ariniello <mark.ariniello@gmail.com>
2015-09-18 12:47:00 -0600

    from Mac

--- Assignment3.py
+++ Assignment3.py
@@ -1,7 +1,31 @@
 import sys 
 
 with open(sys.argv[1], "r") as world:
-    maze = [x.strip('\n').spilt(' ')  for x in world.readlines()] 
+    maze = []
+    for line in world.readlines(): 
+        maze.append(line.rstrip('\n').split(' ')) 
          
 print maze 
 
+class Node: 
+
+    def __init__(self, x, y):
+        self.parent = None 
+        self.toHere = None 
+        self.toGo = None 
+        self.x = x
+        self.y = y 
+        self.cost = None 
+
+class Graph: 
+
+    def __init__(self, maze):
+        self.maze = maze
+        self.start = Node(7,0)
+        self.end = Node(0, 7)
+
+    def astar(self, graph, start, end): 
+        curropen.append(start)
+        closed = []
+        while len(curropen) != 0: 
+        
